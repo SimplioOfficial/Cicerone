ea4c47ac78e582bf219712752fbf986ade5cbe4a
ciripel <nedelcu.alexandru@yahoo.com>
2021-09-02 00:35:02 +0300

    initial version

---
+++ README.md
@@ -0,0 +1,11 @@
+## Cicerone
+[![](https://img.shields.io/github/pipenv/locked/python-version/ciripel/cicerone)]() [![](https://img.shields.io/github/pipenv/locked/dependency-version/ciripel/cicerone/discord-py)]() [![](https://img.shields.io/github/license/ciripel/heimdall)](LICENSE) [![](https://img.shields.io/github/issues/SimplioOfficial/cicerone)](https://github.com/ciripel/Heimdall/issues) [![](https://img.shields.io/github/issues-closed/SimplioOfficial/cicerone)](https://github.com/ciripel/Heimdall/issues)
+### Discord bot for Simplio https://simplio.io/
+
+
+v.0.1.0 - deployed 02.09.2021
+- initial version
+- implemented these commands:
+  help, about, ban (cicerone only), del (Amitabha only)
+- joining verification implemented
+- cicerone members name protection implemented
---
+++ bot.py
@@ -0,0 +1,169 @@
+#!/usr/bin/env python3
+# Work with Python 3.7+
+
+import logging
+import json
+
+import discord
+
+
+logging.basicConfig(format="%(asctime)s | %(levelname)s:%(name)s:%(message)s",
+                    filename='cicerone.log', level=logging.INFO)
+logging.info('----- Started -----')
+
+with open("auth.json") as data_file:
+    auth = json.load(data_file)
+with open("links.json") as data_file:
+    data = json.load(data_file)
+
+
+TOKEN = auth["token"]
+BOT_PREFIX = "!"
+TICKER = "SIO"
+
+intents = discord.Intents(messages=True, guilds=True)
+intents.reactions = True
+intents.members = True
+intents.emojis = True
+intents.presences = True
+intents.typing = False
+
+client = discord.Client(intents=intents)
+
+
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
+
+@client.event
+async def on_message(msg):
+    # We do not want the bot to respond to Bots or Webhooks
+    if msg.author.bot:
+        return
+    # We want the bot to not answer to messages that have no content
+    # (example only attachment messages)
+    # Bot checks BOT_PREFIX
+    if not msg.content or msg.content[0] != BOT_PREFIX:
+        return
+    # Bot ignore all system messages
+    if msg.type is not discord.MessageType.default:
+        return
+
+    args = msg.content[1:].split()
+    cmd = args[0].lower()
+
+    # Bot runs in #bot-commands channel and private channels for everyone
+    # Bot runs in all channels for specific roles
+    if not (
+        isinstance(msg.channel, discord.DMChannel)
+        or msg.channel.name == "bot-commands"
+        or "Cicerone" in [role.name for role in msg.author.roles]
+    ):
+        message = f"{data['default']}"
+        await msg.channel.send(message)
+        return
+
+    # -------- <help> --------
+    elif cmd == "help":
+        message = "\n".join(data["help"])
+    # -------- <about> --------
+    elif cmd == "about":
+        message = "\n".join(data["about"])
+    # -------- <ban(Cicerone rank only)> --------
+    elif (
+        cmd == "ban"
+        and isinstance(msg.channel, discord.TextChannel)
+        and "cicerone" in [role.name for role in msg.author.roles]
+    ):
+        if len(args) < 2:
+            message = (
+                f"Input a substring of users to ban, like `!ban SiO` will ban all users containing"
+                + f" `SiO` in their names (_fuction is case-sensitive_)."
+            )
+            await msg.channel.send(message)
+            return
+        cmd1 = args[1]
+        member = discord.utils.find(
+            lambda m: cmd1 in m.name, msg.channel.guild.members)
+        if member is None:
+            count = 0
+        else:
+            count = 1
+        while not (member is None):
+            await msg.channel.guild.ban(member)
+            member = discord.utils.find(
+                lambda m: cmd1 in m.name, msg.channel.guild.members)
+            if not (member is None):
+                count += 1
+        message = f"Banned {count} members! Nice!"
+    # -------- <del(Amitabha only)> --------
+    elif (
+        cmd == "del"
+        and isinstance(msg.channel, discord.TextChannel)
+        and msg.author.id == 359782573066551320
+    ):
+        if len(args) < 2:
+            message = "Enter the number of messages to delete"
+            await msg.channel.send(message)
+            return
+        cmd1 = args[1]
+        deleted = await msg.channel.purge(limit=int(cmd1))
+        message = f"Deleted {len(deleted)} message(s)"
+
+    else:
+        message = f"{data['unknown']}"
+
+    await msg.channel.send(message)
+
+
+@client.event
+async def on_raw_reaction_add(payload):
+    guild = client.get_guild(859581142159065128)
+    role = guild.get_role(882347739357249576)
+    mbr = guild.get_member(payload.user_id)
+
+    if (payload.message_id == 882670328097169458 and payload.emoji.name == "Simplio"):
+        await mbr.add_roles(role)
+
+
+@client.event
+async def on_raw_reaction_remove(payload):
+    guild = client.get_guild(859581142159065128)
+    role = guild.get_role(882347739357249576)
+    mbr = guild.get_member(payload.user_id)
+
+    if (payload.message_id == 882670328097169458 and payload.emoji.name == "Simplio"):
+        await mbr.remove_roles(role)
+
+
+@client.event
+async def on_member_join(mbr):
+    for ban_word in data["banned_words"]:
+        if mbr.guild.get_member(mbr.id) is not None and ban_word in mbr.name:
+            await mbr.ban()
+            logging.warning(f'Banned {mbr.name} with id: {mbr.id}')
+            return
+
+
+@client.event
+async def on_member_update(before, after):
+    # Bot ignore all members that have MEMBER_ID in ignored_ids list
+    if after.id in data["ignored_ids"]:
+        return
+    for ban_word in data["banned_words"]:
+        if after.guild.get_member(after.id) is not None and ban_word in after.name:
+            await after.ban()
+            logging.warning(f'Banned {after.name} with id: {after.id}')
+            return
+
+
+@client.event
+async def on_ready():
+    print(f"Logged in as: {client.user.name} {{{client.user.id}}}")
+
+client.run(TOKEN)
+logging.info('----- Finished -----')
